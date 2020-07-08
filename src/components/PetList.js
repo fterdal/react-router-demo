f01ece9fd2e6f77d48c6ffc02c4ebd0bc7483b19
Finn <finnterdal@gmail.com>
2020-07-07 23:27:39 -0500

    demo prep done

--- src/components/PetList.js
+++ src/components/PetList.js
@@ -10,14 +10,26 @@ const cody = {
 
 // PetList only renders one SinglePet. We'd like it to render a list of pets,
 // passed in as props.pets. Don't forget to add a unique key to each one!
-const PetList = () => {
-  return (
-    <>
-      <div className="pet-list">
-        <SinglePet pet={cody} />
-      </div>
-    </>
-  )
+class PetList extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      filter: 'all'
+    }
+  }
+
+  render() {
+    const { pets } = this.props;
+    return (
+      <>
+        <div className="pet-list">
+          {pets.map(pet => (
+            <SinglePet key={pet.id} pet={pet} />
+          ))}
+        </div>
+      </>
+    )
+  }
 }
 
 export default PetList
--- src/components/SinglePet.js
+++ src/components/SinglePet.js
@@ -1,7 +1,29 @@
 import React from "react"
 
-const SinglePet = () => {
-  return <div className="single-pet"></div>
+class SinglePet extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      adopted: false
+    }
+    this.handleClick = this.handleClick.bind(this)
+  }
+  handleClick() {
+    this.setState({ adopted: !this.state.adopted })
+  }
+  render() {
+    const { pet } = this.props;
+    const { adopted } = this.state;
+    return (
+      <div className={`single-pet ${adopted && 'adopted'}`}>
+        <div>{pet.name}</div>
+        <div>{pet.description}</div>
+        <div>{pet.species}</div>
+        <div>{adopted ? 'Adopted!' : 'Available'}</div>
+        <button onClick={this.handleClick}>Toggle Status</button>
+      </div>
+    )
+  }
 }
 
 export default SinglePet
